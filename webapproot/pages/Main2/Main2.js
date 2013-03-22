95851175e9da1881ea0175e33118b9201049a806
michaelkantor <mkantor@vmware.com>
2013-03-22 07:49:05 -0700

    Fixes loading of node list to show the dialog of nodes for the selected disposition/leaf-node

--- webapproot/pages/Main2/Main2.js
+++ webapproot/pages/Main2/Main2.js
@@ -9,15 +9,33 @@ dojo.declare("Main2", wm.Page, {
   
     
 
-   
+   /*
     getNodesLVarSuccess: function(inSender, inDeprecated) {        
         var node = inSender.getData()[0];
         var data = [{question: node.question, answer: node.answer}];
-        while (node.node) {
-            node = node.node;
-            data.unshift({question: node.question, answer: node.answer});
+        this.nodeListVar.addItem(data);
+        if (node.parentId) {
+            wm.onidle(this, function() {
+                inSender.filter.setValue("nodeId", node.parentId);
+            });
         }
-        this.nodeListVar.setData(data);
     },
-    _end: 0
+    */
+    getNodeListSVarSuccess: function(inSender, inDeprecated) {
+        if (!this.nodeListVar.isUpdating()) this.nodeListVar.beginUpdate();
+		var node = inSender.getData()[0];
+        var data = {question: node.question, answer: node.answer};
+        this.nodeListVar.addItem(data);
+        if (node.parent_id) {
+            wm.onidle(this, function() {
+                inSender.input.setValue("nodeId", node.parent_id);
+                inSender.update();
+            });
+        } else {
+            this.nodeListVar.endUpdate();
+            this.nodeListVar.setData(this.nodeListVar.getData().reverse());
+            this.nodeListVar.notify();
+        }
+	},
+	_end: 0
 });
