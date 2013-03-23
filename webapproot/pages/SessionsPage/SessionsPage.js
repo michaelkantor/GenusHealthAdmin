91d5510eb054375fb896472292031015c93e8f52
michaelkantor <mkantor@vmware.com>
2013-03-22 21:18:10 -0700

    Adds filter by user

--- webapproot/pages/SessionsPage/SessionsPage.js
+++ webapproot/pages/SessionsPage/SessionsPage.js
@@ -4,5 +4,24 @@ dojo.declare("SessionsPage", wm.Page, {
 	},
 	"preferredDevice": "desktop",
 
+	toCSVButtonClick: function(inSender) {
+		this.sessionsGrid.showCSVData();
+	},
+	sessionsGridSelect1: function(inSender) {
+		var selectedPatientId = inSender.selectedItem.getValue("patientId");
+        var currentPatientId = this.filterSessionsSVar.input.getValue("patientId");
+        if (this.showUserToggle.clicked) {
+            if (selectedPatientId != currentPatientId) {
+                this.filterSessionsSVar.input.setValue("patientId", selectedPatientId);
+            }
+        } else if (currentPatientId) {
+            this.filterSessionsSVar.input.setValue("patientId", null);
+        }
+	},
+	showUserToggleClick: function(inSender) {
+		if (!this.sessionsGrid.selectedItem.isEmpty()) {
+            this.sessionsGridSelect1(this.sessionsGrid);
+		}
+	},
 	_end: 0
 });
